30ce60703097ae2f667a4eaec213e1691f838121
elijahgott <gottelijah@gmail.com>
2024-02-15 16:32:34 -0600

    its ninja

--- lab1.java
+++ lab1.java
@@ -1,6 +1,6 @@
 public class lab1{
 
     public static void main(String[] args) {
-        System.out.println("yoooo waddup chat");
+        System.out.println("yoooo waddup chat its me ninja");
     }
 }
